814109804e0a50a11d78eef9576a425ea2cc8073
gkdlvjzl <48200179+gkdlvjzl@users.noreply.github.com>
2022-06-15 19:00:44 +0900

    Add files via upload
    지정 포인트 두곳 와따가따하기 시간설정으로 해야함

---
+++ MoveAnimlas.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAnimlas : MonoBehaviour
+{
+    [SerializeField] GameObject moveStart;
+    [SerializeField] GameObject moveEnd;
+    public int speed;
+
+    bool A = true;
+
+    private void Start()
+    {
+        InvokeRepeating("Atrue", 2f, 4f);
+    }
+
+    void Update()
+    {
+        Loop();
+        Loop2();
+    }     
+
+    private void Loop()
+    {
+        float StartCos = Vector3.Distance(transform.position, moveStart.transform.position);
+
+        float step = speed * Time.deltaTime;
+
+        if (StartCos >= 1f && A)
+        {
+            transform.LookAt(moveStart.transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, moveStart.transform.position, step);
+        }   
+    }
+    private void Loop2()
+    {
+        float EndCos = Vector3.Distance(transform.position, moveEnd.transform.position);
+
+        float step = speed * Time.deltaTime;
+
+        if(EndCos >= 1 && !A)
+        {
+        transform.LookAt(moveEnd.transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, moveEnd.transform.position, step);
+        }
+    }
+
+    private void Atrue()
+    {
+        if (A)
+        {
+            A = false;
+        }
+        else
+        {
+            A = true;
+        }
+    }
+}
