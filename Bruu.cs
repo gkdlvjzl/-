e362e1f171760ce92d113deccf07ceb2e0490c0f
gkdlvjzl <48200179+gkdlvjzl@users.noreply.github.com>
2022-06-15 19:01:33 +0900

    Add files via upload
    지정 지점 순차로 간뒤 오브젝트 삭제

---
+++ Bruu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bruu : MonoBehaviour
+{
+    public GameObject[] movePoint;
+    public int Lastsize;
+
+    [SerializeField] int index;
+    [SerializeField] float speed;
+
+
+    void Update()
+    {
+        if (index >= Lastsize)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Loop();
+        }
+
+    }
+    private void Loop()
+    {             
+        float nextCos = Vector3.Distance(transform.position, movePoint[index].transform.position);
+        
+        float step = speed * Time.deltaTime;
+        if (nextCos < 0.5f && movePoint != null)
+        {
+            index++;
+        }
+        else
+        {
+        transform.LookAt(movePoint[index].transform.position);
+        transform.position = Vector3.Lerp(transform.position, movePoint[index].transform.position, step);
+
+        //Vector3.Lerp or movetowards중 lerp 첫스타트가 부드럽게 출발하는데 이걸로 조정해야할까?
+        }
+    }
+}
